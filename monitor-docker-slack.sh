2ae1ee28dcb0a03f4ba4f491a5ea870b41c25c2c
Rafael Sena <rafaelsenna13@gmail.com>
2020-08-15 01:56:31 -0300

    changed docker-slack script to use sh instead

--- monitor-docker-slack.sh
+++ monitor-docker-slack.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -ex
-python /monitor-docker-slack.py  --check_interval "$CHECK_INTERVAL" \
+#!/bin/sh
+python monitor-docker-slack.py  --check_interval "$CHECK_INTERVAL" \
        --slack_webhook_url "$SLACK_WEBHOOK_URL" --whitelist "$WHITE_LIST" \
        --msg_prefix "$MSG_PREFIX"
 ## File : monitor-docker-slack.sh ends
